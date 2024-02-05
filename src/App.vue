187c667c995a68501aeb3c2ae70b26e780f5349e
Lucas Magno <lucasamagno@gmail.com>
2024-02-04 21:12:12 -0500

    changes from first night

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,28 @@ export default {
 </script>
 
 <style>
+#app {
+  margin: 0 auto;
+  max-width: 800px; 
+}
+
+
 h1 {
   font-family: 'Roboto', sans-serif;
+  text-align: center;
+  font-size: 45px;
+}
+
+h2 {
+  font-family: 'Lora', sans-serif;
+}
+
+h3, h4 {
+  font-family: 'Manrope', sans-serif;
+}
+
+p, li {
+  font-family: 'Manrope', sans-serif;
+  font-size: 14px;
 }
 </style>
