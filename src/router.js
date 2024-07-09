a15ec071509ebe71068c902eb9bf794af0043d36
Lucas Magno <lucasamagno@gmail.com>
2024-07-09 12:56:41 -0400

    FutureLeadersPageV0.5

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import ExecutiveCoaching from '@/components/ExecutiveCoaching.vue';
 import ForTeamsPage from '@/components/ForTeamsPage.vue';
 import RisingStarsPage from '@/components/RisingStarsPage.vue'
 import StartupsPage from '@/components/StartupsPage.vue'
+import FutureLeadersGrant from './components/FutureLeadersGrant.vue';
 
 const routes = [
   { path: '/', component: HomePage },
@@ -13,12 +14,13 @@ const routes = [
   { path: '/contact', component: ContactPage },
   { path: '/executive-coaching', component: ExecutiveCoaching },
   { path: '/for-teams', component: ForTeamsPage },
+  { path: '/the-future-leaders-grant', component: FutureLeadersGrant },
   { path: '/rising-professionals', component: RisingStarsPage },
   { path: '/startups', component: StartupsPage }  
 ];
 
 const router = createRouter({
-  history: createWebHistory('/'), // Adjusted to include the base URL
+  history: createWebHistory('/'),
   routes,
   scrollBehavior(to) {
     if (to.hash) {
